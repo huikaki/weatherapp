403b5b3fa7fdb303bbc1579ed0c08689b5ba02b3
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-04-12 13:59:24 +0800

    day51

---
+++ src/Exercise/Exercise.js
@@ -0,0 +1,7 @@
+/**
+ * See the Readme.md
+ *
+ * Use this create-react-app to make a JSX element, make the element in this file, Exercise.js
+ * Edit the index.js or App.js in the src folder to display your new element, you will need to import the element
+ *
+ */
