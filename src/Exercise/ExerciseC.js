73038129a787cfd74a6c6793c6de25ee72c7e53d
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-04-14 12:35:32 +0800

    update

--- src/Exercise/ExerciseC.js
+++
@@ -1,6 +0,0 @@
-// import Card & Button from bootstrap
-// Remeber to use the Components in the return statement.
-
-export function BootstrapComponent () {
-  return <>{/* // Your code here */}</>
-}
